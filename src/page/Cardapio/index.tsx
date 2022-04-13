b83c79c12c89ea781aef063f91aed98d17583fac
lazaroconceicao <joselazaroc@gmail.com>
2022-04-12 21:02:53 -0300

    Working filter

--- src/page/Cardapio/index.tsx
+++ src/page/Cardapio/index.tsx
@@ -1,11 +1,12 @@
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
-import { useState } from "react";
+import React, { useState } from "react";
 import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
   return (
     <main>
       <nav className={styles.menu}>
@@ -18,7 +19,7 @@ export default function Cardapio() {
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
         <div className={styles.cardapio__friltros}>
-          <Filtros />
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
       </section>
     </main>
