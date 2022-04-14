d1373198b5902da1b88a86a969f4d71e7e45e45e
lazaroconceicao <joselazaroc@gmail.com>
2022-04-14 15:59:01 -0300

    Inserting items

--- src/page/Cardapio/index.tsx
+++ src/page/Cardapio/index.tsx
@@ -4,6 +4,7 @@ import Buscador from "./Buscador";
 import React, { useState } from "react";
 import Filtros from "./Filtros";
 import Ordenador from "./Ordenador";
+import Itens from "./Itens";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -20,10 +21,11 @@ export default function Cardapio() {
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
-        <div className={styles.cardapio__friltros}>
+        <div className={styles.cardapio__filtros}>
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   );
