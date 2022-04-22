a0bbcce19bde9bb687c084b3aaf512361cbed845
lazaroconceicao <joselazaroc@gmail.com>
2022-04-22 15:53:45 -0300

    Update default page

--- src/page/Inicio/index.tsx
+++ src/page/Inicio/index.tsx
@@ -1,6 +1,7 @@
-import Menu from 'components/Menu';
 import cardapio from 'data/cardapio.json';
 import styles from './Inicio.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+import nossaCasa from 'assets/nossa_casa.png';
 
 export default function Inicio() {
   let pratosRecomendados = [...cardapio];
@@ -9,7 +10,7 @@ export default function Inicio() {
     .slice(0, 3);
   return (
     <section>
-      <h3 className={styles.titulo}>Recomendaçoões da cozinha</h3>
+      <h3 className={stylesTema.titulo}>Recomendaçoões da cozinha</h3>
       <div className={styles.recomendados}>
         {pratosRecomendados.map((item) => (
           <div key={item.id} className={styles.recomendado}>
@@ -19,6 +20,13 @@ export default function Inicio() {
             <button className={styles.recomendado__botao}>Ver mais</button>
           </div>
         ))}
+      </div>
+      <h3 className={stylesTema.titulo}>Nossa Casa</h3>
+      <div className={styles.nossaCasa}>
+        <img src={nossaCasa} alt="Casa do aluroni" />
+        <div className={styles.nossaCasa__endereco}>
+          Rua Vergueiro 3285, <br /> <br /> Vila Mariana - SP
+        </div>
       </div>
     </section>
   );
